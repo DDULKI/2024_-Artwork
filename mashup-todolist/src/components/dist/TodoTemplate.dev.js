7eb7beb57f048a1fc8d47f607616bdf93e89763c
DDULKI <seulki4994@naver.com>
2024-07-22 15:31:21 +0900

    todolist

---
+++ mashup-todolist/src/components/dist/TodoTemplate.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  width: 512px\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+var TodoTemplateBlock = _styledComponents["default"].div(_templateObject());
+//# sourceMappingURL=TodoTemplate.dev.js.map
